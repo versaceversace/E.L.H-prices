d5e97d3b360016bd2039bf27c701fe6a4b478a8d
Aaron M <aaronelf@gmail.com>
2017-02-21 11:39:05 -0400

    working on main gogo site

---
+++ js/gogo.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.gogo1').pushpin({
+    top: 63,
+    bottom: 800,
+    offset: 0
+  });
+  $('.gogo2').pushpin({
+    top: 300,
+    bottom: 800,
+    offset: 0
+  });
+  $('.gogo3').pushpin({
+    top: 300,
+    bottom: 800,
+    offset: 0
+  });
+});
