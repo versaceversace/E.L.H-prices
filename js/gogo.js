a643b07be130d96b7d4c549560e4d0182a3b5ede
Aaron M <aaronelf@gmail.com>
2017-03-14 11:03:23 -0400

    gogo1 almost there

--- js/gogo.js
+++ js/gogo.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
-  $('.gogo1').pushpin({
-    top: 63,
-    bottom: 800,
-    offset: 0
-  });
-  $('.gogo2').pushpin({
-    top: 300,
-    bottom: 800,
-    offset: 0
-  });
-  $('.gogo3').pushpin({
-    top: 300,
-    bottom: 800,
-    offset: 0
-  });
-});
+   $('.tooltipped').tooltip({delay: 50, tooltip: 'some text', html: true});
+
+ });
