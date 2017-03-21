fadf2d42279d1060cb0d53048102d9a6ef136385
Aaron M <aaronelf@gmail.com>
2017-03-20 22:42:01 -0400

    implemented animate.css - reworked design a bit.

--- js/gogo.js
+++ js/gogo.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
    $('.tooltipped').tooltip({delay: 50, tooltip: 'some text', html: true});
    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
  });
+ var options = [
+   {selector: '#staggered-test', offset: 400, callback: function(el) {
+   Materialize.showStaggeredList($(el));
+ } },
+ {selector: '#iconset', offset: 300, callback: function(el) {
+  Materialize.showStaggeredList($(el));
+} }
+];
+Materialize.scrollFire(options);
