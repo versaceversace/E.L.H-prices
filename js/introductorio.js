dffc9947f3e14275c39193fa96ef1c1c935d88e5
Aaron M <aaronelf@gmail.com>
2017-04-03 11:37:56 -0400

    compressed imgs, fixed footer

--- js/introductorio.js
+++ js/introductorio.js
@@ -2,3 +2,18 @@ $(document).ready(function(){
    $('.tooltipped').tooltip({delay: 50, tooltip: 'some text', html: true});
    $('.parallax').parallax();
  });
+ var options = [
+   {selector: '#first', offset: 400, callback: function(el) {
+   Materialize.showStaggeredList($(el));
+ } },
+ {selector: '#second', offset: 300, callback: function(el) {
+  Materialize.showStaggeredList($(el));
+} },
+{selector: '#third', offset: 300, callback: function(el) {
+ Materialize.showStaggeredList($(el));
+} },
+{selector: '#fourth', offset: 300, callback: function(el) {
+ Materialize.showStaggeredList($(el));
+} }
+];
+Materialize.scrollFire(options);
