3e821db71023792c8b535d066b1a0c55ff746a11
Aaron M <aaronelf@gmail.com>
2017-09-05 12:33:07 -0400

    angularjs work on gogo site

--- includes/main.js
+++ includes/main.js
@@ -1,5 +1,5 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
     $scope.mainPrice = "110.000";
-    $scope.mainPriceSabatinos = "130.000";
+    $scope.lastName = "Doe";
 });
